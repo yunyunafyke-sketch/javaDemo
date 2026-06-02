9c8dfaad8e6668c676db0a23baa80005182f3eaf
test-chenweili <yunyunafyke@gmail.com>
2026-06-02 23:29:58 +0800

    Update test controller route

--- src/main/java/com/afyke/demo/controller/TestController.java
+++ src/main/java/com/afyke/demo/controller/TestController.java
@@ -8,7 +8,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/test")
 public class TestController {
 
     @GetMapping
